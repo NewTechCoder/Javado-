768de79ba84d806fcd16ebaab756b9c504c98a2e
aylatshjhb024 <aylatshjhb024@student.wethinkcode.co.za>
2025-02-10 13:59:25 +0200

    Done Iteration 1 and 2

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,25 +1,26 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaskManager {
-    private List<String> tasks; // hint: will change in iteration 3
+    private final List<String> tasks; // hint: will change in iteration 3
 
     public TaskManager() {
-        // Initialize tasks list
+        this.tasks = new ArrayList<>();
     }
 
     public void addTask(String task) {
-        throw new UnsupportedOperationException("Implement this method!");
+        this.tasks.add(task);
     }
 
     public List<String> listTasks() {
-        throw new UnsupportedOperationException("Implement this method!");
+        return this.tasks;
     }
 
     public void deleteTask(String task){
-//        leave for iteration 4
+        this.tasks.remove(task);
     }
 
     public void exit() {
-        // leave for iteration 2
+        this.tasks.clear();
     }
 }
